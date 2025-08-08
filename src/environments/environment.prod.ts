eb7cf474c9d7199cb54ef5c4059ffe9e4225d169
Im4yam9G <israelml64@hotmail.com>
2025-08-08 11:25:31 -0700

    setting prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  environment: "local"
+  environment: "prod",
+  clientId:
+    "441710587933-o28p6ici53uclbea0rjveo6gcdvkuaes.apps.googleusercontent.com",
+  api: "https://tu-api-productiva.com/api/",
+  filenameCsv: "prod-pos",
 };

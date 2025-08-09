61706ba647f89761981d31e9dbb63f6b5bd7d4d0
Im4yam9G <israelml64@hotmail.com>
2025-08-08 18:59:43 -0700

    production environment api modidied

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   environment: "prod",
   clientId:
     "754838360448-m5ujif3mclikif2sti2hoocu2r4ltubp.apps.googleusercontent.com",
-  api: "https://tu-api-productiva.com/api/",
+  api: "https://localhost:2023/api/",
   filenameCsv: "prod-pos",
 };

0918a0b44dba66d5e7f9cbf04e47fa09b7ace006
Im4yam9G <israelml64@hotmail.com>
2025-08-08 19:03:19 -0700

    productin environment new correction

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   environment: "prod",
   clientId:
     "754838360448-m5ujif3mclikif2sti2hoocu2r4ltubp.apps.googleusercontent.com",
-  api: "https://localhost:2023/api/",
+  api: "http://localhost:2023/api/",
   filenameCsv: "prod-pos",
 };
